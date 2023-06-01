1907560b201530161e7f7c7127a32d7b83a3a54a
Andrei Paleshka <andrepoleshko@gmail.com>
2023-06-01 11:04:23 +0200

    fix: refresh access token

--- src/components/axiosHeaders.js
+++ src/components/axiosHeaders.js
@@ -2,7 +2,7 @@ const headers = {
   "x-secret-key": 'GEU4nvd3rej*jeh.eqp',
   "x-api-app-id": 'v3.r.137440105.ffdbab114f92b821eac4e21f485343924a773131.06c3bdbb8446aeb91c35b80c42ff69eb9c457948',
   "Content-Type": 'application/json',
-  "Authorization": "Bearer v3.r.137440105.8cafb95046470c4eb353291b2be0fd5f7a04c6b3.a72183dd97422cd217c3477d62f4a138bbdb2026"
+  "Authorization": "Bearer v3.r.137440105.e6ae789e104bb2d331a033cefc4cf2b718ba5f44.91a5437c771ee6df682fc32e4f266ec6ecd465e0"
 };
 
 export default headers;
